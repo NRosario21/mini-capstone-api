37d8ae8fb4ef826a92c9ea3570a726ac0dda0a81
NRosario21 <nosrea@gmail.com>
2024-07-31 13:12:15 -0500

    Updated Tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,9 +1,12 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create(name: "Test", email: "test@test.com", password: "password", admin: true)
+    post "/sessions.json", params: { email: "test@test.com", password: "password" }
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
 
   test "index" do
     get "/products.json"
@@ -13,38 +16,61 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_equal Product.count, data.length
   end
 
-  test "create" do
-    assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test product", price: 1, image_url: "image.jpg", description: "test description", supplier_id: Supplier.first.id }
-    end
-  end
-
   test "show" do
     get "/products/#{Product.first.id}.json"
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "tax", "total", "supplier", "is_discounted?", "image_url", "description", "created_at", "updated_at"], data.keys
+    assert_equal ["created_at", "description", "id", "image_url", "is_discounted?", "name", "price", "supplier", "tax", "total", "updated_at"].sort, data.keys.sort
   end
+
+  test "create" do
+    assert_difference "Product.count", 1 do
+      post "/products.json",
+        params: { name: "test", price: 10, image_url: "test.jpg", description: "test description", supplier_id: Supplier.first.id },
+        headers: { "Authorization" => "Bearer #{@jwt}" }
+      assert_response 200
+    end
+
+    assert_difference "Product.count", 0 do
+      post "/products.json",
+        params: {},
+        headers: { "Authorization" => "Bearer #{@jwt}" }
+      assert_response 422
+    end
+
+    post "/products.json", params: {}
+    assert_response 401
+  end
+
   test "update" do
     product = Product.first
-    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+    patch "/products/#{product.id}.json",
+      params: { name: "Updated name" },
+      headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
     assert_equal product.price.to_s, data["price"]
-    assert_equal product.image_url, data["image_url"]
     assert_equal product.description, data["description"]
 
-    patch "/products/#{product.id}.json", params: { name: "" }
+    patch "/products/#{product.id}.json",
+      params: { name: "" },
+      headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 422
+
+    patch "/products/#{product.id}.json"
+    assert_response 401
   end
 
   test "destroy" do
     assert_difference "Product.count", -1 do
-      delete "/products/#{Product.first.id}.json"
+      delete "/products/#{Product.first.id}.json", headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
+
+    delete "/products/#{Product.first.id}.json"
+    assert_response 401
   end
 end
